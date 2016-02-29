ab1461bdc50493bd42e3513cc57105db4cc5391d
YOUR NAME <harrison.c.chu@gmail.com>
2016-02-29 11:09:17 -0800

    Hooking everything up together

--- maps_bot_server.py
+++ maps_bot_server.py
@@ -2,18 +2,23 @@ from flask import Flask
 from flask import request
 import os
 import simple_twilio_response
+import directions_service
 
 app = Flask(__name__)
+directions_service = directions_service.DirectionsService()
 
 @app.route('/directions', methods=['GET', 'POST'])
 def directions():
 	#Twilio sends the SMS data via POST
+	directions = "No directions available"
 	if request.method == 'POST':
 		print(request.form['Body'])	
+		user_input = request.form['Body']
+		directions = directions_service.get_directions(user_input)
 
 	#return this so twilio doesn't through an error
 	response = simple_twilio_response.SimpleTwilioResponse()
-	response.set_message("this has been successful!")
+	response.set_message(directions)
 	return response.print_xml()
 
 if __name__ == '__main__':
