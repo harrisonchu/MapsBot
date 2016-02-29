5e3259938b2e43a5aad17c10260c3adf425de5ad
YOUR NAME <harrison.c.chu@gmail.com>
2016-02-29 10:19:12 -0800

    Testing twilio functionality

--- maps_bot_server.py
+++ maps_bot_server.py
@@ -3,10 +3,14 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/directions")
+@app.route('/directions', methods=['GET', 'POST'])
 def directions():
-	return "Hello World!"
+	#Twilio sends the SMS data via POST
+	if request.method == 'POST':
+		print(request.form['Body'])	
+	else:
+		return "Hello World!"
 
-if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
