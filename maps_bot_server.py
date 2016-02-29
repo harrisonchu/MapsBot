6c216044935e063ff6ac0ef7baea9dabb8713d07
YOUR NAME <harrison.c.chu@gmail.com>
2016-02-29 10:02:35 -0800

    getting server ready to run on heroku

--- maps_bot_server.py
+++ maps_bot_server.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/directions")
 def directions():
-    return "Hello World!"
+	return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
